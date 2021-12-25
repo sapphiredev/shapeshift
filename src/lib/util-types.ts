e7f46cc2d2af6cdbd97cad9a926f2f23f3a22ad3
Jeroen Claassens <support@favware.tech>
2021-12-25 09:04:03 +0100

    Update src/lib/util-types.ts

--- src/lib/util-types.ts
+++ src/lib/util-types.ts
@@ -6,8 +6,21 @@ import type { BaseValidator } from '../validators/BaseValidator';
  */
 export type Constructor<T> = (new (...args: readonly any[]) => T) | (abstract new (...args: readonly any[]) => T);
 
+/**
+ * @internal
+ * This type is only used inside of the library and is not exported on the root level
+ */
 export type Type<V> = V extends BaseValidator<infer T> ? T : never;
 
+/**
+ * @internal
+ * This type is only used inside of the library and is not exported on the root level
+ */
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type NonNullObject = {} & object;
+
+/**
+ * @internal
+ * This type is only used inside of the library and is not exported on the root level
+ */
 export type MappedObjectValidator<T> = { [key in keyof T]: BaseValidator<T[key]> };
