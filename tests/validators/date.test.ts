c94da417330d9c33e3d51314eda031799054e297
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-02-25 21:52:28 +0530

    eq and ne date

--- tests/validators/date.test.ts
+++ tests/validators/date.test.ts
@@ -59,4 +59,28 @@ describe('DateValidator', () => {
 			expect(() => gePredicate.parse(value)).toThrow(new ConstraintError('s.date.ge', 'Invalid Date value', value, 'expected >= 2022-01-01'));
 		});
 	});
+
+	describe('eq', () => {
+		const eqPredicate = s.date.eq(new Date('2022-01-02'));
+
+		test.each([new Date('2022-01-02')])('GIVEN %s THEN returns given value', (value) => {
+			expect(eqPredicate.parse(value)).toBe(value);
+		});
+
+		test.each([new Date('2021-01-02'), new Date('2023-01-02')])('GIVEN %s THEN throws ConstraintError', (value) => {
+			expect(() => eqPredicate.parse(value)).toThrow(new ConstraintError('s.date.eq', 'Invalid Date value', value, 'expected = 2022-01-02'));
+		});
+	});
+
+	describe('ne', () => {
+		const nePredicate = s.date.ne(new Date('2022-01-02'));
+
+		test.each([new Date('2021-01-02'), new Date('2023-01-02')])('GIVEN %s THEN returns given value', (value) => {
+			expect(nePredicate.parse(value)).toBe(value);
+		});
+
+		test.each([new Date('2022-01-02')])('GIVEN %s THEN throws ConstraintError', (value) => {
+			expect(() => nePredicate.parse(value)).toThrow(new ConstraintError('s.date.ne', 'Invalid Date value', value, 'expected != 2022-01-02'));
+		});
+	});
 });
