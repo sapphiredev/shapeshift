dd41fe9b01edfd62ff5ecf58bfe7d6fbdcecf75b
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-02-25 21:59:51 +0530

    fix: some more tests

--- tests/validators/set.test.ts
+++ tests/validators/set.test.ts
@@ -3,11 +3,11 @@ import { CombinedError, s, ValidationError } from '../../src';
 describe('SetValidator', () => {
 	const predicate = s.set(s.string);
 
-	test("GIVEN a value which isn't a set THEN throws ValidationError", () => {
-		expect(() => predicate.parse(123)).toThrow(new ValidationError('SetValidator', 'Expected a set', 123));
+	test.each([123, 'foo', [], {}, new Map()])("GIVEN a value which isn't a set %s THEN throws ValidationError", (input) => {
+		expect(() => predicate.parse(input)).toThrow(new ValidationError('SetValidator', 'Expected a set', input));
 	});
 
-	test.each(['1', 'a', 'foo'])('GIVEN a set with string value %s THEN returns the given value', (input) => {
+	test.each(['1', 'a', 'foo'])('GIVEN a set with string value %s THEN returns the given set', (input) => {
 		expect(predicate.parse(new Set().add(input))).toStrictEqual(new Set().add(input));
 	});
 
