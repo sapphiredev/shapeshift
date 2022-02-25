2a96f5ac0c17984daa9f6fc7462eefd21e650307
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-02-25 21:57:31 +0530

    fix: update set test

--- tests/validators/set.test.ts
+++ tests/validators/set.test.ts
@@ -3,17 +3,17 @@ import { CombinedError, s, ValidationError } from '../../src';
 describe('SetValidator', () => {
 	const predicate = s.set(s.string);
 
-	test('GIVEN a set with string value THEN returns the given value', () => {
-		expect(predicate.parse(new Set().add('b'))).toStrictEqual(new Set().add('b'));
-	});
-
 	test("GIVEN a value which isn't a set THEN throws ValidationError", () => {
 		expect(() => predicate.parse(123)).toThrow(new ValidationError('SetValidator', 'Expected a set', 123));
 	});
 
-	test('Given a set with non-string value THEN throw CombinedError', () => {
-		const set = new Set().add(123);
+	test.each(['1', 'a', 'foo'])('GIVEN a set with string value %s THEN returns the given value', (input) => {
+		expect(predicate.parse(new Set().add(input))).toStrictEqual(new Set().add(input));
+	});
 
-		expect(() => predicate.parse(set)).toThrow(new CombinedError([new ValidationError('StringValidator', 'Expected a string', 123)]));
+	test.each([123, [], {}])('Given a set with non-string value %s THEN throw CombinedError', (input) => {
+		const set = new Set().add(input);
+
+		expect(() => predicate.parse(set)).toThrow(new CombinedError([new ValidationError('StringValidator', 'Expected a string', input)]));
 	});
 });
