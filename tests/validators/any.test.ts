97e67ea81c33c03ed791078c44939c320cc4adfa
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-02-26 15:19:56 +0530

    unknown test

--- tests/validators/any.test.ts
+++
@@ -1,9 +0,0 @@
-import { s } from '../../src';
-
-describe('PassThroughValidator', () => {
-	const predicate = s.any;
-
-	test.each([1, 'hello', null])('GIVEN anything %s THEN returns the given value', (input) => {
-		expect(predicate.parse(input)).toBe(input);
-	});
-});
