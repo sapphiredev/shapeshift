ac1556c081e3f0265ccac08d13160da37efa1c8f
Antonio Román <kyradiscord@gmail.com>
2022-03-13 15:27:01 +0100

    tests: add coverage for `combinedErrorFn`

---
+++ tests/util/common/combinedResultFn.test.ts
@@ -0,0 +1,132 @@
+import { combinedErrorFn } from '../../../src/constraints/util/common/combinedResultFn';
+
+describe('combinedErrorFn', () => {
+	describe('0 functions', () => {
+		test('GIVEN no functions THEN always returns null', () => {
+			const fn = combinedErrorFn();
+			expect(fn()).toBe(null);
+		});
+	});
+
+	describe('1 function', () => {
+		test('GIVEN one function returning null THEN returns null', () => {
+			const cb = jest.fn().mockReturnValue(null);
+			const fn = combinedErrorFn(cb);
+
+			expect(fn('foo', 'bar')).toBe(null);
+
+			expect(cb).toBeCalledTimes(1);
+			expect(cb).toHaveBeenCalledWith('foo', 'bar');
+		});
+
+		test('GIVEN one function returning error THEN returns the same error', () => {
+			const error = new Error('my precious');
+			const cb = jest.fn().mockReturnValue(error);
+			const fn = combinedErrorFn(cb);
+
+			expect(fn('foo', 'bar')).toBe(error);
+
+			expect(cb).toBeCalledTimes(1);
+			expect(cb).toHaveBeenCalledWith('foo', 'bar');
+		});
+	});
+
+	describe('2 functions', () => {
+		test('GIVEN (null, null) THEN returns null', () => {
+			const cb0 = jest.fn().mockReturnValue(null);
+			const cb1 = jest.fn().mockReturnValue(null);
+			const fn = combinedErrorFn(cb0, cb1);
+
+			expect(fn('foo', 'bar')).toBe(null);
+
+			expect(cb0).toBeCalledTimes(1);
+			expect(cb0).toHaveBeenCalledWith('foo', 'bar');
+
+			expect(cb1).toBeCalledTimes(1);
+			expect(cb1).toHaveBeenCalledWith('foo', 'bar');
+		});
+
+		test('GIVEN (null, error) THEN returns error', () => {
+			const error = new Error('not all those who wander are lost');
+			const cb0 = jest.fn().mockReturnValue(null);
+			const cb1 = jest.fn().mockReturnValue(error);
+			const fn = combinedErrorFn(cb0, cb1);
+
+			expect(fn('foo', 'bar')).toBe(error);
+
+			expect(cb0).toBeCalledTimes(1);
+			expect(cb0).toHaveBeenCalledWith('foo', 'bar');
+
+			expect(cb1).toBeCalledTimes(1);
+			expect(cb1).toHaveBeenCalledWith('foo', 'bar');
+		});
+
+		test('GIVEN (error, null) THEN returns error', () => {
+			const error = new Error('it is a dangerous business');
+			const cb0 = jest.fn().mockReturnValue(error);
+			const cb1 = jest.fn().mockReturnValue(null);
+			const fn = combinedErrorFn(cb0, cb1);
+
+			expect(fn('foo', 'bar')).toBe(error);
+
+			expect(cb0).toBeCalledTimes(1);
+			expect(cb0).toHaveBeenCalledWith('foo', 'bar');
+
+			expect(cb1).not.toHaveBeenCalled();
+		});
+	});
+
+	describe('3 functions', () => {
+		test('GIVEN (null, null, null) THEN returns null', () => {
+			const cb0 = jest.fn().mockReturnValue(null);
+			const cb1 = jest.fn().mockReturnValue(null);
+			const cb2 = jest.fn().mockReturnValue(null);
+			const fn = combinedErrorFn(cb0, cb1, cb2);
+
+			expect(fn('foo', 'bar')).toBe(null);
+
+			expect(cb0).toBeCalledTimes(1);
+			expect(cb0).toHaveBeenCalledWith('foo', 'bar');
+
+			expect(cb1).toBeCalledTimes(1);
+			expect(cb1).toHaveBeenCalledWith('foo', 'bar');
+
+			expect(cb2).toBeCalledTimes(1);
+			expect(cb2).toHaveBeenCalledWith('foo', 'bar');
+		});
+
+		test('GIVEN (null, error, null) THEN returns error', () => {
+			const error = new Error('go where you must go, and hope!');
+			const cb0 = jest.fn().mockReturnValue(null);
+			const cb1 = jest.fn().mockReturnValue(error);
+			const cb2 = jest.fn().mockReturnValue(null);
+			const fn = combinedErrorFn(cb0, cb1, cb2);
+
+			expect(fn('foo', 'bar')).toBe(error);
+
+			expect(cb0).toBeCalledTimes(1);
+			expect(cb0).toHaveBeenCalledWith('foo', 'bar');
+
+			expect(cb1).toBeCalledTimes(1);
+			expect(cb1).toHaveBeenCalledWith('foo', 'bar');
+
+			expect(cb2).not.toHaveBeenCalled();
+		});
+
+		test('GIVEN (error, null, null) THEN returns error', () => {
+			const error = new Error('all is well that ends better');
+			const cb0 = jest.fn().mockReturnValue(error);
+			const cb1 = jest.fn().mockReturnValue(null);
+			const cb2 = jest.fn().mockReturnValue(null);
+			const fn = combinedErrorFn(cb0, cb1, cb2);
+
+			expect(fn('foo', 'bar')).toBe(error);
+
+			expect(cb0).toBeCalledTimes(1);
+			expect(cb0).toHaveBeenCalledWith('foo', 'bar');
+
+			expect(cb1).not.toHaveBeenCalled();
+			expect(cb2).not.toHaveBeenCalled();
+		});
+	});
+});
