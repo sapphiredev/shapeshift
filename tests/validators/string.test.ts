b6122fb203e5d31c0cf300bc7554301295852e8e
Jeroen Claassens <support@favware.tech>
2022-03-13 14:03:22 +0100

    chore: lolwhat?
    permalink: http://whatthecommit.com/549e1c8fffbc7605222ad006ff600c4f

--- tests/validators/string.test.ts
+++ tests/validators/string.test.ts
@@ -123,7 +123,8 @@ describe('StringValidator', () => {
 				'with@multiple-@-signs',
 				"skywalker@did-you-ever-hear-the-tragedy-of-Darth-Plagueis-The-Wise?-I-thought-not.-It's-not-a-story-the-Jedi-would-tell-you.-It's-a-Sith-legend.-Darth-Plagueis-was-a-Dark-Lord-of-the-Sith,-so-powerful-and-so-wise-he-could-use-the-Force-to-influence-the-midichlorians-to-create-life…-He-had-such-a-knowledge-of-the-dark-side-that-he-could-even-keep-the-ones-he-cared-about-from-dying.-The-dark-side-of-the-Force-is-a-pathway-to-many-abilities-some-consider-to-be-unnatural.-He-became-so-powerful…-the-only-thing-he-was-afraid-of-was-losing-his-power,-which-eventually,-of-course,-he-did.-Unfortunately,-he-taught-his-apprentice-everything-he-knew,-then-his-apprentice-killed-him-in-his-sleep.-Ironic.-He-could-save-others-from-death,-but-not-himself.com",
 				'When-you-look-at-the-dark-side,-careful-you-must-be.-For-the-dark-side-looks-back@master-yoda-swamp.com',
-				'short-account-name@domain-name-that-has-more-than-sixty-three-characters-and-is-then.followed-by-another-segment-that-got-split-by-a-full-stop-symbol.com'
+				'short-account-name@domain-name-that-has-more-than-sixty-three-characters-and-is-then.followed-by-another-segment-that-got-split-by-a-full-stop-symbol.com',
+				`foo@bar.${'a'.repeat(64)}`
 			])('GIVEN %p THEN throws a ConstraintError', (input) => {
 				expectError(
 					() => emailPredicate.parse(input),
