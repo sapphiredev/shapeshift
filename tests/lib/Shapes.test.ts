7584014094e74ac714c305a9f4b865367b373b9d
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-03-21 08:08:38 +0530

    chore: remove unnc file

--- tests/lib/Shapes.test.ts
+++
@@ -1,9 +0,0 @@
-import { s } from '../../src';
-
-describe('Type tests', () => {
-	test('GIVEN string validator in Unwrap THEN return string validator', () => {
-		const predicate = s.string;
-
-		expectt;
-	});
-});
