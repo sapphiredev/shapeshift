1b729078be32a88aeddf574c9cff3098990d4f94
Vlad Frangu <kingdgrizzle@gmail.com>
2022-05-10 00:15:02 +0300

    feat(StringValidator): add date string checks (#106)
    Co-authored-by: A. Román <kyradiscord@gmail.com>

--- src/constraints/StringConstraints.ts
+++ src/constraints/StringConstraints.ts
@@ -13,7 +13,8 @@ export type StringConstraintName =
 			| 'url'
 			| 'uuid'
 			| 'email'
-			| `ip${'v4' | 'v6' | ''}`}`;
+			| `ip${'v4' | 'v6' | ''}`
+			| 'date'}`;
 
 export type StringProtocol = `${string}:`;
 
@@ -137,4 +138,23 @@ export function stringUuid({ version = 4, nullable = false }: StringUuidOptions 
 	);
 	const expected = `expected to match UUID${typeof version === 'number' ? `v${version}` : ` in range of ${version}`}`;
 	return stringRegexValidator('s.string.uuid', expected, regex);
+}
+
+export function stringDate(): IConstraint<string> {
+	return {
+		run(input: string) {
+			const time = Date.parse(input);
+
+			return Number.isNaN(time)
+				? Result.err(
+						new ExpectedConstraintError(
+							's.string.date',
+							'Invalid date string',
+							input,
+							'expected to be a valid date string (in the ISO 8601 or ECMA-262 format)'
+						)
+				  )
+				: Result.ok(input);
+		}
+	};
 }
