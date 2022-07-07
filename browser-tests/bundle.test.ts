3f752cfab2d1f6188046ba3995ee08618f09faa4
parbez <imranbarbhuiya.fsd@gmail.com>
2022-07-07 19:42:29 +0530

    ci: push unfinished test

---
+++ browser-tests/bundle.test.ts
@@ -0,0 +1,35 @@
+import { Window } from 'happy-dom';
+
+describe('bundle-test', () => {
+	let window: Window;
+
+	beforeEach(() => {
+		window = new Window();
+		const { document } = window;
+
+		document.write(`
+				<html>
+					<head>
+						<title>Test page</title>
+					</head>
+					<body>
+						<script src="../dist/index.global.js"/>
+					</body>
+				</html>
+		`);
+	});
+
+	afterEach(() => {
+		vi.clearAllMocks();
+	});
+
+	test('GIVEN an unique array THEN return the given value', () => {
+		expect(window.SapphireShapeshift.s.string.array.unique.parse(['a', 'b', 'c'])).toStrictEqual(['a', 'b', 'c']);
+	});
+});
+
+declare module 'happy-dom' {
+	interface Window {
+		SapphireShapeshift: typeof import('../src');
+	}
+}
--- vitest.browser.config.ts
+++ vitest.browser.config.ts
@@ -3,11 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
-		coverage: {
-			enabled: true,
-			reporter: ['text', 'lcov', 'clover']
-		},
-		dir: './tests'
+		environment: 'happy-dom',
+		dir: './browser-tests'
 	},
 	esbuild: {
 		target: 'es2020'
