615b7cfb15aa71888596e585bf529f0d2d8b7025
Jeroen Claassens <support@favware.tech>
2022-07-08 22:40:57 +0200

    chore: fix pc load letter error
    permalink: http://whatthecommit.com/43398194a4cdd602ab7456be8066106f

--- tests/browser/browser.test.ts
+++ tests/browser/browser.test.ts
@@ -2,39 +2,41 @@
  * @vitest-environment jsdom
  */
 
-import { readFileSync } from 'node:fs';
-import { DOMWindow, JSDOM } from 'jsdom';
-import path from 'node:path';
-
-const script = readFileSync(path.join(__dirname, '../../dist/index.global.js'), 'utf8');
-
-declare global {
-	interface Window {
-		SapphireShapeshift: typeof import('../../src');
-	}
-	namespace JSX {
-		interface Element {
-			outerHTML: string;
-		}
-	}
-}
-
-let window: DOMWindow;
+import { type DOMWindow, JSDOM } from 'jsdom';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
 
 describe('browser-bundle-test', () => {
-	beforeEach(() => {
-		window = new JSDOM('', {
-			runScripts: 'dangerously'
-		}).window;
+	let window: DOMWindow;
 
-		const { document } = window;
-
-		const scriptTag = document.createElement('script');
-		scriptTag.textContent = script;
-		document.head.appendChild(scriptTag);
+	beforeAll(async () => {
+		window = new JSDOM(
+			`
+				<!DOCTYPE html>
+				<html lang="en">
+				<head>
+					<meta charset="UTF-8">
+					<meta http-equiv="X-UA-Compatible" content="IE=edge">
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+					<title>BrowserBundleTest</title>
+					<script>${await readFile(join(__dirname, '../../dist/index.global.js'), 'utf8')}</script>
+				</head>
+				<body></body>
+				</html>
+		`,
+			{
+				runScripts: 'dangerously'
+			}
+		).window;
 	});
 
 	test('GIVEN an unique array THEN return the given value', () => {
 		expect(window.SapphireShapeshift.s.string.parse('Hello')).toBe('Hello');
 	});
 });
+
+declare global {
+	interface Window {
+		SapphireShapeshift: typeof import('../../src');
+	}
+}
