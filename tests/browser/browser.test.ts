92d05d83c1fbab53f98f61219fb01d49fc031bae
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-07-10 21:00:54 +0530

    fix: ensure browser compatibility (#150)
    * chore: maybe we are now 100% browser compitible?
    
    * chore: why is everything broken
    
    permalink: http://whatthecommit.com/61392d8529ad51956024f721ebf30995
    
    * chore: epic
    
    permalink: http://whatthecommit.com/d9796047a759e445935a287c17d1037c
    
    * chore: is there an award for this?
    
    permalink: http://whatthecommit.com/cb4f7f1124e8bd95fc10f272830701d3
    
    * ci: push unfinished test
    
    * chore: merge pull my finger request
    
    permalink: http://whatthecommit.com/569aa58b98f6e4aac466357a4997c2b2
    
    * ci: do in correct way
    
    * ci: fix scripts and build once
    
    * ci: skip lib check bcz of happy-dom typings
    
    * ci: change name
    
    * test: fix file path
    
    * chore: use jsdom
    
    * chore: it worked
    
    * ci: change test as vitest is weired
    
    * chore: remove script
    
    * chore: fix pc load letter error
    
    permalink: http://whatthecommit.com/43398194a4cdd602ab7456be8066106f
    
    * chore: reinventing the wheel. again.
    
    permalink: http://whatthecommit.com/b2b30ca13c6041aee39eecb2b3a47211
    
    * chore: readd `node:` protocol with a patch
    
    * chore: it worked https://github.com/yarnpkg/berry/issues/4231
    
    Co-authored-by: Jeroen Claassens <support@favware.tech>

---
+++ tests/browser/browser.test.ts
@@ -0,0 +1,42 @@
+/**
+ * @vitest-environment jsdom
+ */
+
+import { type DOMWindow, JSDOM } from 'jsdom';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
+describe('browser-bundle-test', () => {
+	let window: DOMWindow;
+
+	beforeAll(async () => {
+		window = new JSDOM(
+			`
+				<!DOCTYPE html>
+				<html lang="en">
+				<head>
+					<meta charset="UTF-8">
+					<meta http-equiv="X-UA-Compatible" content="IE=edge">
+					<meta name="viewport" content="width=device-width, initial-scale=1.0">
+					<title>BrowserBundleTest</title>
+					<script>${await readFile(join(__dirname, '../../dist/index.global.js'), 'utf8')}</script>
+				</head>
+				<body></body>
+				</html>
+		`,
+			{
+				runScripts: 'dangerously'
+			}
+		).window;
+	});
+
+	test('GIVEN an unique array THEN return the given value', () => {
+		expect(window.SapphireShapeshift.s.string.parse('Hello')).toBe('Hello');
+	});
+});
+
+declare global {
+	interface Window {
+		SapphireShapeshift: typeof import('../../src');
+	}
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup';
+import NodeModulesPolyfills from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig({
 	clean: true,
@@ -6,13 +7,14 @@ export default defineConfig({
 	entry: ['src/index.ts'],
 	format: ['esm', 'cjs', 'iife'],
 	minify: false,
-	skipNodeModulesBundle: false,
-	bundle: true,
+	skipNodeModulesBundle: true,
 	sourcemap: true,
 	target: 'es2020',
 	tsconfig: 'src/tsconfig.json',
 	keepNames: true,
 	globalName: 'SapphireShapeshift',
+	// @ts-expect-error it's exported as default
+	esbuildPlugins: [NodeModulesPolyfills.default()],
 	esbuildOptions: (options, context) => {
 		if (context.format === 'cjs') {
 			options.banner = {
