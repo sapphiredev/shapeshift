f909c85c665b5b81301388d0667579991b733c6a
Jeroen Claassens <support@favware.tech>
2022-07-10 16:44:19 +0200

    build: use tsup `dts` option
    Seeing as our tests do not need composite builds we can disable that
    in src and then enable `dts` in tsup to make the build process
    quicker and easier

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
 	clean: true,
-	dts: false,
+	dts: true,
 	entry: ['src/index.ts'],
 	format: ['esm', 'cjs', 'iife'],
 	minify: false,
