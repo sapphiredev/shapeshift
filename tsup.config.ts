9bb1ff95f2852e765a27c7a839ff5145ee6e3a01
Jeroen Claassens <support@favware.tech>
2023-12-04 21:39:07 +0100

    fix: properly split CJS and ESM

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,37 @@
-import { defineConfig } from 'tsup';
 import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
+import { defineConfig, type Options } from 'tsup';
 
-export default defineConfig({
+const baseOptions: Options = {
 	clean: true,
 	dts: true,
 	entry: ['src/index.ts'],
-	format: ['esm', 'cjs', 'iife'],
 	minify: false,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
 	target: 'es2020',
 	tsconfig: 'src/tsconfig.json',
 	keepNames: true,
-	globalName: 'SapphireShapeshift',
-	esbuildPlugins: [nodeModulesPolyfillPlugin()],
 	treeshake: true
-});
+};
+
+export default [
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/cjs',
+		format: 'cjs',
+		outExtension: () => ({ js: '.cjs' })
+	}),
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/esm',
+		format: 'esm'
+	}),
+	defineConfig({
+		...baseOptions,
+		globalName: 'SapphireShapeshift',
+		esbuildPlugins: [nodeModulesPolyfillPlugin()],
+		dts: false,
+		outDir: 'dist/iife',
+		format: 'iife'
+	})
+];
