770fe57743fec6202af3400260f8137eb2220329
Jeroen Claassens <support@favware.tech>
2024-05-02 21:43:49 +0200

    test: better configure vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,11 @@ export default defineConfig({
 	test: {
 		globals: true,
 		coverage: {
+			provider: 'v8',
 			enabled: true,
-			reporter: ['text', 'lcov', 'cobertura']
+			reporter: ['text', 'lcov', 'cobertura'],
+			include: ['src/**/*.ts'],
+			exclude: ['src/constraints/base/IConstraint.ts', 'src/constraints/type-exports.ts']
 		}
 	},
 	esbuild: {
