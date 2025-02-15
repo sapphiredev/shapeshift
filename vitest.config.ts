1330f774e3ce1fbc1d35e4c61c046d36a2f4f324
Jeroen Claassens <support@favware.tech>
2025-02-15 23:26:02 +0100

    test: remove browser testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 		includeTaskLocation: true,
 		globals: true,
 		coverage: {
-			provider: 'istanbul',
+			provider: 'v8',
 			enabled: true,
 			reporter: ['text', 'lcov'],
 			include: ['src/**/*.ts'],
